7bcde76191d52f4f7e848a77b8e8e3cdf4a6c0e9
amotor-AM <66324211+amotor-AM@users.noreply.github.com>
2020-09-28 13:54:10 -0700

    changed everything

--- landing_page.js
+++ landing_page.js
@@ -8,13 +8,19 @@ function menuToggle() {
   nav.classList.toggle("active");
 }
 
+// Type Words
+
+//Grabs the spans and sets them to an array of words
 const words = document.querySelector(".words").children;
+// Sets output to span typewords
 const output = document.querySelector(".typeWords");
 
+//setting default values
 let wordIndex = 0;
 let charIndex = 0;
 let textArray;
 
+// sets value of const 'text' to an array that contains the children of the span "words" from the HTML element. This is split by individual letters. I.E. ["E", "X", "A", "M", "P", "L", "E"]. Finally we will run the typing function.
 function type() {
   const text = words[wordIndex].getAttribute("data-text");
   textArray = text.split("");
@@ -22,6 +28,7 @@ function type() {
   typing();
 }
 
+// This is what types out our letters and outputs them onto the page. We start by creating a span and setting the value to the index of the textArray we are currently on. We then append the typeWords span with the current value of the createed span. We will then incrament the span by 1. We then can create a for loop and say "if the length of the created span is the same as the textArray then we can move onto the next step." The next loop checks to see if the word we are outputting is the last word from the child of the HTML element "words". If it is the word index is reset, if not we just move onto the next word and move on.
 function typing() {
   if (charIndex == textArray.length) {
     charIndex = 0;
@@ -42,4 +49,5 @@ function typing() {
   }
 }
 
+// Loads on window load
 window.onload = type();
